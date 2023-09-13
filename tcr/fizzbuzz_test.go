0f4dfc91a38ad07a3b2feb08cad478c1832aca98
Fredele20 <victorisholaoladele@gmail.com>
2023-09-13 11:55:14 +0100

    WIP

--- tcr/fizzbuzz_test.go
+++ tcr/fizzbuzz_test.go
@@ -44,3 +44,9 @@ func (s *FizzBuzzSuite) TestFifteenChangedFizzBuzz() {
 
 	s.Equal([]string{"FizzBuzz"}, r)
 }
+
+func (s *FizzBuzzSuite) TestManyNumbers() {
+	r := fizzbuzz.Run([]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15})
+
+	s.Equal([]string{"1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz"}, r)
+}
