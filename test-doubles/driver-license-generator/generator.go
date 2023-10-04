4b85891bcbc2087b4b46fddfb1fc7e959f63ef0f
Fredele20 <victorisholaoladele@gmail.com>
2023-10-04 03:32:09 +0100

    added mocks and fake test doubles

--- test-doubles/driver-license-generator/generator.go
+++ test-doubles/driver-license-generator/generator.go
@@ -1,30 +1,48 @@
 package driverlicensegenerator
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+)
 
 type Applicant interface {
 	IsOver17() bool
 	HoldsLicense() bool
+	GetInitials() string
+	GetDOB() string
 }
 
 type Logger interface {
 	LogStuff(v string)
 }
 
+type RandomNumberGenerator interface {
+	GetRandomNumbers(len int) string
+}
+
 type NumberGenerator struct{
 	l Logger
+	r RandomNumberGenerator
 }
 
-func NewNumberGenerator(l Logger) *NumberGenerator {
-	return &NumberGenerator{l}
+func NewNumberGenerator(l Logger, r RandomNumberGenerator) *NumberGenerator {
+	return &NumberGenerator{l, r}
 }
 
-func (n *NumberGenerator) Generate(a Applicant) (string, error) {
+func (g *NumberGenerator) Generate(a Applicant) (string, error) {
 	if a.HoldsLicense() {
-		n.l.LogStuff("Duplicate license attempted, cannot create another license")
+		g.l.LogStuff("Duplicate license attempted, cannot create another license")
 		return "", errors.New("Duplicate license attempted, cannot create another license")
 	}
 
-	n.l.LogStuff("Underaged Applicant, must be over 17 to get a license")
-	return "", errors.New("Underaged Applicant, must be over 17 to get a license")
+	if !a.IsOver17() {
+		g.l.LogStuff("Underaged Applicant, must be over 17 to get a license")
+		return "", errors.New("Underaged Applicant, must be over 17 to get a license")
+	}
+
+	n := fmt.Sprintf("%s%s", a.GetInitials(), a.GetDOB())
+
+	num := 16 - len(n)
+
+	return fmt.Sprintf("%s%s", n, g.r.GetRandomNumbers(num)), nil
 }
--- test-doubles/driver-license-generator/generator_test.go
+++ test-doubles/driver-license-generator/generator_test.go
@@ -2,6 +2,7 @@ package driverlicensegenerator_test
 
 import (
 	driverlicensegenerator "go-tdd/test-doubles/driver-license-generator"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/suite"
@@ -14,8 +15,9 @@ type DrivingLicenseSuite struct {
 func (s *DrivingLicenseSuite) TestUnderageApplicant() {
 	a := UnderageApplicant{}
 	l := &SpyLogger{}
+	r := FakeRand{}
 
-	lg := driverlicensegenerator.NewNumberGenerator(l)
+	lg := driverlicensegenerator.NewNumberGenerator(l, r)
 	_, err := lg.Generate(a)
 
 	s.Error(err)
@@ -28,8 +30,9 @@ func (s *DrivingLicenseSuite) TestUnderageApplicant() {
 func (s *DrivingLicenseSuite) TestNoSecondLicense() {
 	a := LicenseHolderApplicant{}
 	l := &SpyLogger{}
+	r := FakeRand{}
 
-	lg := driverlicensegenerator.NewNumberGenerator(l)
+	lg := driverlicensegenerator.NewNumberGenerator(l, r)
 	_, err := lg.Generate(a)
 
 	s.Error(err)
@@ -39,11 +42,66 @@ func (s *DrivingLicenseSuite) TestNoSecondLicense() {
 	s.Contains(l.lastMessage, "Duplicate")
 }
 
+func (s *DrivingLicenseSuite) TestLicenseGenerator() {
+	a := ValidApplicant{"MDB", "04102023"}
+	l := &SpyLogger{}
+	r := FakeRand{}
+
+	lg := driverlicensegenerator.NewNumberGenerator(l, r)
+	ln, err := lg.Generate(a)
+
+	s.NoError(err)
+	s.Equal("MDB0410202300000", ln)
+}
+
+func (s *DrivingLicenseSuite) TestLicenseGeneratorShorterInitials() {
+	a := ValidApplicant{"MB", "04102023"}
+	l := &SpyLogger{}
+	r := FakeRand{}
+
+	lg := driverlicensegenerator.NewNumberGenerator(l, r)
+	ln, err := lg.Generate(a)
+
+	s.NoError(err)
+	s.Equal("MB04102023000000", ln)
+}
+
 func TestDrivingLicenseSuite(t *testing.T) {
 	suite.Run(t, new(DrivingLicenseSuite))
 }
 
+type ValidApplicant struct {
+	initials string
+	dob      string
+}
+
+func (v ValidApplicant) IsOver17() bool {
+	return true
+}
+
+func (v ValidApplicant) HoldsLicense() bool {
+	return false
+}
+
+func (v ValidApplicant) GetInitials() string {
+	return v.initials
+}
+
+func (v ValidApplicant) GetDOB() string {
+	return v.dob
+}
+
 type UnderageApplicant struct{}
+
+// GetDOB implements driverlicensegenerator.Applicant.
+func (u UnderageApplicant) GetDOB() string {
+	return ""
+}
+
+// getInitials implements driverlicensegenerator.Applicant.
+func (u UnderageApplicant) GetInitials() string {
+	return ""
+}
 
 func (u UnderageApplicant) IsOver17() bool {
 	return false
@@ -55,12 +113,27 @@ func (u UnderageApplicant) HoldsLicense() bool {
 
 type LicenseHolderApplicant struct{}
 
+// GetDOB implements driverlicensegenerator.Applicant.
+func (l LicenseHolderApplicant) GetDOB() string {
+	return ""
+}
+
 func (l LicenseHolderApplicant) IsOver17() bool {
 	return true
 }
 
 func (l LicenseHolderApplicant) HoldsLicense() bool {
 	return true
+}
+
+func (l LicenseHolderApplicant) GetInitials() string {
+	return ""
+}
+
+type FakeRand struct {}
+
+func (f FakeRand) GetRandomNumbers(len int) string {
+	return strings.Repeat("0", len)
 }
 
 type SpyLogger struct {
